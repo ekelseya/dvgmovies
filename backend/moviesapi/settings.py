1e583ae0a9e2a4b93ca4359d91604c1fbabb4258
ekelseya <42783392+ekelseya@users.noreply.github.com>
2022-06-24 04:56:05 -0600

    Adjust naming convention

--- backend/moviesapi/settings.py
+++ backend/moviesapi/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'reviews',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = 'moviesapi.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = 'moviesapi.wsgi.application'
 
 
 # Database
