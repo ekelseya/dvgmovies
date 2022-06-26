32e8f5e460d8e25ddd665685f976ea69b572d601
ekelseya <42783392+ekelseya@users.noreply.github.com>
2022-06-26 10:53:18 -0600

    Typo fixes

--- backend/moviesapi/settings.py
+++ backend/moviesapi/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'reviews',
-    'graphene-django',
+    'graphene_django',
 ]
 
 MIDDLEWARE = [
--- backend/reviews/schema.py
+++ backend/reviews/schema.py
@@ -1,6 +1,5 @@
 import graphene
 from django.contrib.auth import get_user_model
-from django.db.models import Prefetch
 from graphene_django import DjangoObjectType
 from reviews import models
 
