aefcd826cfbf08be2a46ca251c3cd10cd50ff713
ekelseya <42783392+ekelseya@users.noreply.github.com>
2022-07-07 10:53:10 -0600

    Add switch for movie list filter

--- frontend/src/components/MoviesAll.vue
+++ frontend/src/components/MoviesAll.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <h2>All movies</h2>
+    <h2>Filter movies</h2>
+    <div class="switch-field">
+		<input type="radio" id="radio-all" name="switch-two" value="all" v-model="filter" checked/>
+		<label for="radio-all">Show all</label>
+		<input type="radio" id="radio-to-watch" name="switch-two" value="toWatch" v-model="filter" />
+		<label for="radio-to-watch">Watch List</label>
+		<input type="radio" id="radio-watched" name="switch-two" value="watched" v-model="filter" />
+		<label for="radio-watched">Already Seen</label>
+	</div>
       <p v-if="error">Something went wrong...</p>
       <p v-if="loading">Loading...</p>
       <MovieList v-if="result" :movies="result.allMovies" />
@@ -76,3 +84,50 @@ export default {
     },
 }
 </script>
+
+<style>
+.switch-field {
+	display: flex;
+	margin-bottom: 36px;
+	overflow: hidden;
+}
+
+.switch-field input {
+	position: absolute !important;
+	clip: rect(0, 0, 0, 0);
+	height: 1px;
+	width: 1px;
+	border: 0;
+	overflow: hidden;
+}
+
+.switch-field label {
+	background-color: #e4e4e4;
+	color: rgba(0, 0, 0, 1);
+	font-size: 14px;
+	line-height: 1;
+	text-align: center;
+	padding: 8px 16px;
+	margin-right: -1px;
+	border: 1px solid rgba(0, 0, 0, 0.2);
+	box-shadow: inset 0 1px 3px rgba(0, 0, 0, 0.3), 0 1px rgba(255, 255, 255, 0.1);
+	transition: all 0.1s ease-in-out;
+}
+
+.switch-field label:hover {
+	cursor: pointer;
+}
+
+.switch-field input:checked + label {
+	background-color: hsla(160, 100%, 37%, .5);
+	box-shadow: none;
+}
+
+.switch-field label:first-of-type {
+	border-radius: 4px 0 0 4px;
+}
+
+.switch-field label:last-of-type {
+	border-radius: 0 4px 4px 0;
+}
+</style>
