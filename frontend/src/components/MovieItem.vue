1bfef66bc28f6bd1155a3bf63b229b39ccd9fc91
ekelseya <42783392+ekelseya@users.noreply.github.com>
2022-07-12 14:43:43 -0600

    Update MovieItem.vue

--- frontend/src/components/MovieItem.vue
+++ frontend/src/components/MovieItem.vue
@@ -28,13 +28,28 @@
           </li>
         </ul>
       </div>
-      <ul>
-        <li class="movie__reviews" v-for="review in result.movieBySlug.reviewSet" :key="review.title">
-          <span v-if="review.published">
-            <router-link :to="`/reviews/${review.slug}`">Read the review: {{ review.title }}</router-link>
-          </span>
-        </li>
-      </ul>
+      <div class="movie__info">
+        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24"><path d="M12 .288l2.833 8.718h9.167l-7.417 5.389 2.833 8.718-7.416-5.388-7.417 5.388 2.833-8.718-7.416-5.389h9.167z"/></svg>
+        Starring 
+        <ul>
+          <li class="movie__producer" v-for="actor in result.movieBySlug.cast" :key="actor.name">
+            <router-link :to="`/cast/${actor.name}`">
+              {{ actor.name }} 
+            </router-link>
+          </li>
+        </ul>
+      </div>
+      <div class="movie__info">
+        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24"><path d="M15.812 4.819c-.33-.341-.312-.877.028-1.207l3.469-3.365c.17-.164.387-.247.603-.247.219 0 .438.085.604.257l-4.704 4.562zm-5.705 8.572c-.07.069-.107.162-.107.255 0 .194.158.354.354.354.089 0 .178-.033.247-.1l.583-.567-.493-.509-.584.567zm4.924-6.552l-1.994 1.933c-1.072 1.039-1.619 2.046-2.124 3.451l.881.909c1.419-.461 2.442-.976 3.514-2.016l1.994-1.934-2.271-2.343zm5.816-5.958l-5.137 4.982 2.586 2.671 5.138-4.98c.377-.366.566-.851.566-1.337 0-1.624-1.968-2.486-3.153-1.336zm-11.847 12.119h-4v1h4v-1zm9-1.35v1.893c0 4.107-6 2.457-6 2.457s1.518 6-2.638 6h-7.362v-20h12.629l2.062-2h-16.691v24h10.189c3.163 0 9.811-7.223 9.811-9.614v-4.687l-2 1.951z"/></svg>
+        Read the review: 
+        <ul>
+          <li class="movie__reviews" v-for="review in result.movieBySlug.reviewSet" :key="review.title">
+            <span v-if="review.published">
+              <router-link :to="`/reviews/${review.slug}`"> {{ review.title }} </router-link>
+            </span>
+          </li>
+        </ul>
+      </div>
       <ul>
         <li class="movie__genre" v-for="genre in result.movieBySlug.genre" :key="genre.name">
           <router-link :to="`/genre/${genre.name}`">
