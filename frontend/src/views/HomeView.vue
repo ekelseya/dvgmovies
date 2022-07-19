97943ffea95e8ed7d08dee197191ecf2e9eebf8d
ekelseya <42783392+ekelseya@users.noreply.github.com>
2022-07-19 08:05:36 -0600

    Update homepage

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,6 +1,30 @@
 <template>
   <main>
-    <h1 class="home__view">Welcome to the Awesome Movie Review Site!</h1>
-    <p>This is where I'll talk about how awesome movies are and why I made this site.</p>
+    <h1 class="home__view">Miles of Movies</h1>
+    <p>
+      I love movies. New movies, old movies, award winners and cult classics, I love them all. 
+      This website was designed to help me keep track of the movies I've watched and those 
+      that I want to watch. <router-link to="/movies">Movies</router-link> can be filtered 
+      by genre, actor, director, or production company. Every so often I'll jot down a 
+      movie <router-link to="/reviews">review</router-link>. Reviews can be sorted by 
+    </p>
+    <h2>Behind the name <em>Miles of Movies</em></h2>
+    <h3>Footage</h3>
+    <p>Even though most video is digital now, we still refer to footage: </p>
+    <ul>
+      <li>"Roll that beautiful bean footage... "</li>
+      <li>"Dramatic footage of the devastating tornado aftermath... "</li>
+      <li>"Newly released body camera footage reveals... "</li>    
+    </ul>
+    <p>Film is measured in feet, and raw, unedited video was and still is referred to as "footage."</p>
+    <h3>Frame rate and footage</h3>
+    <p>
+      The standard frame rate for movies is 24 frames per second. The most common film format, 
+      35 mm, has a frame size of 36 by 24 mm. Doing some quick back of the napkin math, and an average
+      two hour movie would equal 10,800 feet of film, or more than two miles of film!
+    </p>
+    <p>
+      I have miles and miles of movies to watch, and the list just keeps getting longer.
+    </p>
   </main>
 </template>
