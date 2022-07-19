b95abf1f79d90c047b979b6bd6ca27d22f0d938c
ekelseya <42783392+ekelseya@users.noreply.github.com>
2022-07-19 08:21:44 -0600

    Style updates

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -6,7 +6,7 @@
       This website was designed to help me keep track of the movies I've watched and those 
       that I want to watch. <router-link to="/movies">Movies</router-link> can be filtered 
       by genre, actor, director, or production company. Every so often I'll jot down a 
-      movie <router-link to="/reviews">review</router-link>. Reviews can be sorted by 
+      movie <router-link to="/reviews">review</router-link>. 
     </p>
     <h2>Behind the name <em>Miles of Movies</em></h2>
     <h3>Footage</h3>
@@ -21,7 +21,7 @@
     <p>
       The standard frame rate for movies is 24 frames per second. The most common film format, 
       35 mm, has a frame size of 36 by 24 mm. Doing some quick back of the napkin math, and an average
-      two hour movie would equal 10,800 feet of film, or more than two miles of film!
+      two hour movie would equal 10,800 feet of film, more than two miles long!
     </p>
     <p>
       I have miles and miles of movies to watch, and the list just keeps getting longer.
